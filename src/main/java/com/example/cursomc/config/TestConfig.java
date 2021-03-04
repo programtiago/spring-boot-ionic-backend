c61645ecf826c14c5ba20d4230b1ce384d541e0d
Tiago Silva <programtiago@gmail.com>
2021-03-04 11:24:55 +0000

    Criacao de profile de teste

---
+++ src/main/java/com/example/cursomc/config/TestConfig.java
@@ -0,0 +1,26 @@
+package com.example.cursomc.config;
+
+import com.example.cursomc.service.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import java.text.ParseException;
+
+@Configuration
+@Component
+//@Profile("test")
+public class TestConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Bean
+    public boolean instantiateDatabase() throws ParseException
+    {
+        dbService.instantiateTestDatabase();
+        return true;
+    }
+}
